3de93d2cc8d413a89ec75d5d20909ea2fa02b45d
ivan morenkov <ivan_morenkov@epam.com>
2024-04-24 14:19:10 +0200

    add homework 6 and 7 until paragraph 2

---
+++ src/Homework_6_and_7.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class Homework_6_and_7 {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
--- src/entity/Person.java
+++ src/entity/Person.java
@@ -1,9 +1,48 @@
 package entity;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Person {
     int id;
     String name;
     int age;
     String underlyingDisease;
     String complaining;
+
+    public Person(int id, String name, int age, String underlyingDisease, String complaining) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.underlyingDisease = underlyingDisease;
+        this.complaining = complaining;
+    }
+    File file = new File("output.txt");
+    public static void saveTo (File file, Person person) throws IOException {
+        if (file.exists() && file.isFile()) {
+            try (FileOutputStream stream = new FileOutputStream(file)) {
+                try (PrintWriter writer = new PrintWriter(stream)) {
+                    writer.println(person.id);
+                    writer.println(person.name);
+                    writer.println(person.age);
+                    writer.println(person.underlyingDisease);
+                    writer.println(person.complaining);
+                }
+            }
+        }
+    }
+
+    public static Person loadFrom(Person person, File file) throws IOException {
+        try (FileInputStream stream = new FileInputStream(file)) {
+            try (Scanner scanner = new Scanner(stream)) {
+                return new Person(
+                        new Integer(scanner.nextLine()),
+                        scanner.nextLine(),
+                        new Integer(scanner.nextLine()),
+                        scanner.nextLine(),
+                        scanner.nextLine()
+                );
+            }
+        }
+    }
 }
